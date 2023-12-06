6df3fb03d045c8d44e0d0504292f6feba2b40291
Zack Sirrush <66755256+ZackSirrush@users.noreply.github.com>
2023-12-06 14:49:37 -0600

    Se añade proyecto: Refaccionaria
    Simula una refaccionaria con arreglos, donde permite al usuario añadir refacciones, buscarlas, consultarlas y ordenarlas

---
+++ 18. Refaccionaria/Program.cs
@@ -0,0 +1,71 @@
+﻿using DPRN2_U3_A4_ANNT;
+public class DPRN_U3_A4_ANNT
+{
+    public static void Main(string [] args) //Main
+    {
+        int repetir=1; //Declaración de variables
+        int menu,n;
+        string tmp;
+        bool esnumero;
+
+        Refacciones rf = new Refacciones(); //Invoca la clase refacciones
+
+        do //Mientras repetir sea igual a 1, el ciclo se repite
+        {
+
+            Console.Clear(); //Limpia la pantalla al iniciar cada iteración           
+            do //Previene que un usuario malintencionado ingrese un caractér
+            {
+                Console.WriteLine("*****Refaccionaria automotríz*****"); //Menú para el registro de participantes
+                Console.WriteLine("¿Qué desea realizar?");
+                Console.WriteLine("1. Registrar refacciones");
+                Console.WriteLine("2. Listar refacciones");
+                Console.WriteLine("3. Buscar una refacción específica");
+                Console.WriteLine("4. Ordenar refacciones por nombre");
+                Console.WriteLine("5. Terminar programa");
+                tmp = Console.ReadLine();
+                esnumero = int.TryParse(tmp,out int valor);//Compara si el valor almacenado en tmp es numero
+                Console.Clear(); //Limpia la pantalla
+            } while (!esnumero); //Mientras el valor no sea número, repite el menú
+            
+            menu = Convert.ToInt16(tmp);//Convierte el número almacenado en tmp a int 
+
+            switch (menu) //Switch case para el menú
+            {
+                case 1: //Caso 1, registro de refacciones
+                    Console.WriteLine("*****Registro de refacciones*****");                  
+                    do //Previene que el usuario ingrese letras
+                    {
+                        Console.WriteLine("¿Cuántas refacciones desea registrar?");
+                        tmp = Console.ReadLine();
+                        esnumero = int.TryParse(tmp, out int valor); //Compara si el valor ingresado es número
+                    } while (!esnumero); //Mientras el valor no sea número, el procedimiento se repite
+                    n = Convert.ToInt16(tmp); //Convierte la variable en int
+                    
+                    for (int i = 0; i < n; i++) //Repite el método según la cantidad de registros a ingresar
+                    {
+                        rf.AgregarRefaccion(); //Método de la clase refacciones
+                    }
+                    break;                
+                case 2: //Caso 2, Muestra refacciones registradas
+                    rf.MostrarRefacciones(); //Método de la clase refacciones
+                    break;
+                case 3://Caso 3, Busca la refacción
+                    rf.BuscarRefaccion(); //Método de la clase refacciones
+                    break;
+                case 4: //Caso 4, Ordena las refacciones por nombre
+                    rf.OrdenarRefacciones();//Método de la clase refacciones
+                    break;
+                case 5://Caso 5, Termina el programa
+                    Console.WriteLine("Finalizando programa");
+                    repetir = 2; //Establece el valor de repetir en 2
+                    break;
+                default: //Opción no válida
+                    Console.WriteLine("Error, opción inválida");
+                    Console.ReadLine();
+                    break;
+            }
+
+        } while (repetir == 1); //Mientras repetir sea igual a 1, el programa se repite
+    }    
+}
---
+++ 18. Refaccionaria/Refacciones.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPRN2_U3_A4_ANNT
+{
+    class Refacciones
+    {
+        private string nombre, marca; //Atributos privados
+
+        Dictionary<string, string> refac = new Dictionary<string, string>(); //Declaración del diccionario
+
+        public string Nombre //Getters de la clase
+        {
+            get { return nombre; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+        public void AgregarRefaccion() //Método para agregar refacciones al diccionario
+        {
+            try //Control de errores al insertar elementos
+            {
+                Console.WriteLine("Ingresa el nombre de la refacción");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingresa la marca de la refacción");
+                marca = Console.ReadLine();
+
+                refac.Add(nombre, marca); //Almacena los valores como parte del diccionario
+            }
+            catch (ArgumentException) //Excepción de tipo argumental
+            {
+                Console.WriteLine("Ya existe un elemento con este nombre\n"); //Mensaje al cometer error
+            }
+        }
+
+        public void MostrarRefacciones() //Método para mostrar las refacciones del diccionario 
+        {
+            Console.WriteLine("*****Listado de refacciones registradas*****");
+            Console.WriteLine("Las refacciones registradas son:");
+            Console.WriteLine("Nombre, Marca");
+            foreach(string llave in refac.Keys) //Imprime las refacciones almacenadas, nombre y marca
+            {
+                Console.WriteLine(llave+", "+refac[llave]);
+            }
+            Console.ReadLine(); //Detiene el sistema hasta que el usuario presione una tecla
+        }
+
+        public void BuscarRefaccion()//Método para buscar las refacciones en el diccionario
+        {
+            Console.WriteLine("*****Buscar refacción*****");
+            Console.WriteLine("Ingrese el nombre de la refacción que desea buscar: ");
+            string buscar = Console.ReadLine(); //Ingresa el valor buscado
+
+            if (refac.ContainsKey(buscar)) //Compara si existe el valor en el diccionario
+            {
+                Console.WriteLine("La refacción "+buscar+" existe"); //Sí existe, muestra los datos
+                Console.WriteLine("Su información es: ");
+                Console.WriteLine("Nombre: " + buscar);
+                Console.WriteLine("Marca: " + refac[buscar]);
+            }
+            else
+            {
+                Console.WriteLine("Refacción no registrada, no existe en el catálogo"); //Si no, indica su inexistencia
+            }
+            Console.ReadLine();//Detiene el sistema hasta que el usuario presione una tecla
+
+        }
+
+        public void OrdenarRefacciones() //Método para ordenar las refacciones del diccionario
+        {
+            var ordenar = refac.Keys.ToList(); //Recupera las llaves del diccionario
+            ordenar.Sort();//Ordena las llaves
+            Console.WriteLine("*****Refacciones ordenadas por nombre*****");
+            Console.WriteLine("Nombre, Marca");
+            foreach (string llave in ordenar) //Imp´rime las llaves ordenadas
+            {
+                Console.WriteLine(llave + ", " + refac[llave]);
+            }
+            Console.ReadLine();//Detiene el sistema hasta que el usuario presione una tecla
+        }
+    }
+}
