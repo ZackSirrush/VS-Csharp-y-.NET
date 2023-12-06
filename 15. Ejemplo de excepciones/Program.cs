2101d21739c062e2c1c2745dd07c6ec05d0d4b69
Zack Sirrush <66755256+ZackSirrush@users.noreply.github.com>
2023-12-06 14:34:22 -0600

    Se añade proyecto: Ejemplo de excepciones
    Una pequeña calculadora para dividir que controla excepciones como al dividir 0/0.

---
+++ 15. Ejemplo de excepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DPRN2_U3_A1_ANNT_EjemploExceptions
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, r; ;
+            System.Console.WriteLine("Dividir dos números");
+            try
+            {
+                
+                Console.WriteLine("Digite el primer número: ");
+                n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite el segundo número: ");
+                n2 = int.Parse(Console.ReadLine());
+                r = n1/n2;
+                Console.WriteLine("El resultado de: " + n1 + "/" + n2 + " es " + r);
+            }
+            catch (FormatException sfe) //CONTROLA EXCEPCIONES POR TIPO DE DATO INCORRECTO
+            {
+                Console.WriteLine("Ingrese un valor entero. \n"+sfe.Message);
+            }
+            catch (DivideByZeroException exdpc) //CONTROLA EXCEPCIONES POR DIVISIÓN ENTRE CERO
+            {
+                Console.WriteLine("No se puede dividir entre cero. \n" + exdpc.Message);
+            }
+            finally
+            {
+                Console.ReadKey(true); //Evita el cierre abrupto del sistema
+            }
+            
+
+        }
+    }
+}
+
+
